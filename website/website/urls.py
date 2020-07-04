ae8893be0eaede679c6c5047568bf0d5a7699c8f
AaronDemond <aarondemond@dal.ca>
2020-07-04 12:11:38 -0700

    linked login page, removed redundant pages

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,22 +1,18 @@
-"""website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from .views import index as home_page
+from .views import about as about_page
+from .views import shop as shop_page
+from .views import contact as contact_page
+from .views import account as account_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='index'),
+    path('about', about_page, name='about'),
+    path('shop', shop_page, name='shop'),
+    path('contact', contact_page, name='contact'),
+    path('account', account_page, name='account'),
+    path('email_signup', home_page, name='index')
+
 ]
--- website/website/views.py
+++ website/website/views.py
@@ -4,10 +4,37 @@ from django.template import loader
 
 
 
-
+from django.contrib.auth.models import User
 def index(request):
 	context = { 'data' : 12345 }
+	try:
+		value= request.POST['email']
+		user = User.objects.create_user(value, value, '12345')
+		user.save()
+	except:
+		value = -1
 	return render(request, 'index.html', context)
-    #return HttpResponse("helloworld")
 
+def about(request):
+	context = { 'data' : 12345 }
+
+	return render(request, 'about.html', context)
+	return HttpResponse("about_page")
+
+def shop(request):
+	context = { 'data' : 12345 }
+
+	return render(request, 'shop.html', context)
+	return HttpResponse("shop")
+
+
+def contact(request):
+	context = { 'data' : 12345 }
+	return render(request, 'contact.html', context)
+	return HttpResponse("contact")
+
+def account(request):
+	context = { 'data' : 12345 }
+	return render(request, 'account.html', context)
+	return HttpResponse("account")
 

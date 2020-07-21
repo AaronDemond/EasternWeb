e6cc54ea239c87c90a3acde452ba308a837d8347
Aaron Demond <aarondemond@dal.ca>
2020-07-21 14:35:44 -0700

    Added console output

--- website/website/data_gatherer.py
+++ website/website/data_gatherer.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 import time
 import requests
+#from website.website.models import Signal
 
 print("Gathering BTC market data")
 while True:
-	r=requests.get("http://localhost:8000/trades")
-	r=requests.get("http://localhost:8000/invest")
-	time.sleep(10)
+    print("sending get req")
+    r=requests.get("http://localhost:8000/trades")
+    r=requests.get("http://localhost:8000/invest")
+    print("sleeping")
+    time.sleep(10)
 
 

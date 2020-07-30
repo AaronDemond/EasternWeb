e6a94428f090ed04be4247902ba2de08b0cb3d7d
AaronDemond <aarondemond@dal.ca>
2020-07-30 12:25:13 -0700

    added signal get method

--- website/website/crypto.py
+++ website/website/crypto.py
@@ -1,13 +1,14 @@
+from website.models import Signal
 class Helper():
 	def sort(self,signals):
-		'''Accepts a queryset returns a 
-		   list of sorted btc buy signals 
+		'''Accepts a queryset or list of signals and returns a
+		   list sorted by its quantity
 		'''
 	
 		list_object = []
 		for s in signals:
 			try:
-				if s.qty and s.symbol=="BTCUSDT":
+				if s.qty and s.symbol==signals[0].symbol:
 					list_object.append(s)
 			except:
 				pass
@@ -21,4 +22,21 @@ class Helper():
 
 		list_object.sort(key=getQty,reverse=True)
 		return list_object
+
+
+	def getSignalList(self,symbol='BTCUSDT'):
+		'''returns a list object of the given symbol'''
+		l=[]
+		for s in Signal.objects.all().order_by("-id"):
+			if s.symbol==symbol:
+				l.append(s)
+		return(l)
+
+	def getBTCSignal(self):
+		return("ETH SIGNAL")
+
+	def getBTCSignal(self):
+		return("ETH SIGNAL")
+		
+	
 

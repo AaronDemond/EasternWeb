4c6ef3544ef0f751b4b37a306eb06b2192859472
AaronDemond <aarondemond@dal.ca>
2020-07-30 12:38:30 -0700

    formated output

--- website/website/data_gatherer.py
+++ website/website/data_gatherer.py
@@ -5,24 +5,30 @@ import os
 def cls():
 	os.system('cls' if os.name=='nt' else 'clear')
 
-lc=0
-while True:
-	lc=lc+1
+COUNTER=1
+
+# Signal beggining
+print("EasternWeb Project\nver: 0\n")
+print(datetime.now())
+print("--------------------")
+
+# start script
+while COUNTER:
 	'''Loops indefinately and gathers market data from easternweb '''
 
-	# Signal beggining
-	print("Collecting trade data")
-
 	# get BTC Trade data (500 most recent) and write to db
+	print("Sending request to Binance")
 	r=requests.get("http://localhost:8000/trades") 
 	print("BTC trade-objs > db")
 
 	# get XRP Trade data (500 most recent) and write to db
+	print("Sending request to Binance")
 	r=requests.get("http://localhost:8000/trades2")
 	print("XRP trade-objs > db")
 
 	# Get most recent prices & print to console
 	print("Collecting price data")
+	print("----")
 	r2=requests.get("http://localhost:8000/get-last-price?symbol=BTCUSDT")
 	print("BTC PRICE: " + r2.text)
 	r3=requests.get("http://localhost:8000/get-last-price?symbol=ETHUSDT")
@@ -41,10 +47,11 @@ while True:
 	print("=====================")
 	time.sleep(1)
 
-	# Clear console gui
-	if lc==1:
+	# Clear console gui every second pass
+	COUNTER=COUNTER+1
+	if COUNTER==2:
 		cls()
-		lc=0
+		lc=1
 		print("BTC PRICE: " + r2.text)
 		print("ETH PRICE: " + r3.text)
 		print("DASH PRICE: " + r7.text)

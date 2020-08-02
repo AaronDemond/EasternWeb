9d79bfbbb58c8e9b35eb2fab331a52d327df0c49
AaronDemond <aarondemond@dal.ca>
2020-08-02 14:45:51 -0700

    Added startup bash scripts

---
+++ gatherer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+start website/easternweburl.url
+start website/website/trade-gatherer.py
+start website/website/price-gatherer.py
+echo "Hello"
---
+++ start-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python website/manage.py runserver

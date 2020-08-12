de8ccfdf46689d4277515c2ee7b33ace7d116d11
Aaron Demond <aarondemond@dal.ca>
2020-08-12 17:46:43 -0300

    Update admin.py

--- src/website/admin.py
+++ src/website/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Trade 
-from .models import Signal 
-from .models import HistoricalTrend
-from .models import HistoricalPrice
-from .models import Candle 
+
+from .models import Signal, Trade, HistoricalPrice,
+HistoricalTrend, Candle
+
+# model must be registered 
+# to appear on admin page
+admin.site.register(HistoricalPrice, HPA)
+admin.site.register(Candle, CandleAdmin)
+admin.site.register(HistoricalTrend, HTA)
+admin.site.register(Signal, SignalAdmin)
+admin.site.register(Trade, TradeAdmin)
 
 class HPA(admin.ModelAdmin):
 	pass
@@ -25,9 +31,3 @@ class TradeAdmin(admin.ModelAdmin):
 
 class APAdmin(admin.ModelAdmin):
 	pass
-
-admin.site.register(HistoricalPrice, HPA)
-admin.site.register(Candle, CandleAdmin)
-admin.site.register(HistoricalTrend, HTA)
-admin.site.register(Signal, SignalAdmin)
-admin.site.register(Trade, TradeAdmin)
